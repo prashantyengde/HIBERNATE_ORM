5376f6534cbef03b1a08dae575fbc58473d0c711
prashantyengde <prashantyengde@gmail.com>
2026-03-16 13:42:45 +0530

    HibernateFirst

---
+++ src/main/java/com/telusko/model/Student.java
@@ -0,0 +1,63 @@
+package com.telusko.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name="StudentTable")
+public class Student {
+	
+	@Id
+	@Column(name="SID")
+	private Integer Id;
+	
+	@Column(name="SNAME")
+	private String sname;
+	
+	@Column(name="SCITY")
+	private String scity;
+	
+	public Student() {
+	
+		System.out.println("Zero param COnstructor for Hibernate");
+		
+	}
+	
+	
+
+	
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getScity() {
+		return scity;
+	}
+
+	public void setScity(String scity) {
+		this.scity = scity;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [Id=" + Id + ", sname=" + sname + ", scity=" + scity + "]";
+	}
+	
+	
+
+	
+}
